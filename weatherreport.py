21269ea512a7dd8a379f6a4449b4269009b1a6b8
SCKA-1482 <80355270+SCKA-1482@users.noreply.github.com>
2025-07-30 23:09:12 +0530

    Update weatherreport.py

--- weatherreport.py
+++ weatherreport.py
@@ -1,5 +1,3 @@
-
-
 def sensorStub():
     return {
         'temperatureInC': 50,
@@ -8,37 +6,37 @@ def sensorStub():
         'windSpeedKMPH': 52
     }
 
+def sensorStubHighPrecipLowWind():
+    return {
+        'temperatureInC': 50,
+        'precipitation': 70,  # > 60
+        'humidity': 40,
+        'windSpeedKMPH': 40   # < 50
+    }
 
 def report(sensorReader):
     readings = sensorReader()
     weather = "Sunny Day"
 
-    if (readings['temperatureInC'] > 25):
+    if readings['temperatureInC'] > 25:
         if readings['precipitation'] >= 20 and readings['precipitation'] < 60:
             weather = "Partly Cloudy"
         elif readings['windSpeedKMPH'] > 50:
             weather = "Alert, Stormy with heavy rain"
     return weather
 
-
 def testRainy():
     weather = report(sensorStub)
     print(weather)
-    assert("rain" in weather)
+    assert "rain" in weather, "Expected 'rain' in weather report"
 
-
-def testHighPrecipitation():
-    # This instance of stub needs to be different-
-    # to give high precipitation (>60) and low wind-speed (<50)
-
-    weather = report(sensorStub)
-
-    # strengthen the assert to expose the bug
-    # (function returns Sunny day, it should predict rain)
-    assert(len(weather) > 0);
-
+def testHighPrecipitationLowWind():
+    weather = report(sensorStubHighPrecipLowWind)
+    print(weather)
+    assert "rain" in weather, "Expected 'rain' in weather report when precipitation is high"
 
 if __name__ == '__main__':
     testRainy()
-    testHighPrecipitation()
-    print("All is well (maybe!)");
+    testHighPrecipitationLowWind()  
+    print("All is well (maybe!)")
+
